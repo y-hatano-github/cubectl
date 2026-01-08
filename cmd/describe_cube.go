7e670474c4c015d0e93a437cdbbf4117ec763264
y.hatano <y.hatano.github@gmail.com>
2026-01-08 17:14:21 +0900

    fix describe command

--- cmd/describe_cube.go
+++ cmd/describe_cube.go
@@ -19,6 +19,7 @@ var describeCubeCmd = &cobra.Command{
 		}
 
 		fmt.Printf("Name: %s\n", name)
+		fmt.Printf("Name: %s\n", name)
 		fmt.Println("Namespace: default")
 		fmt.Println("Type: 3D Cube")
 		fmt.Println("Status: Ready")
@@ -26,10 +27,39 @@ var describeCubeCmd = &cobra.Command{
 		fmt.Println("Edges: 12")
 		fmt.Println("Faces: 6")
 		fmt.Println("Rotation: 0,0,0")
-		fmt.Println("Message: This is not kubectl, but cubectl!")
+		fmt.Println("Containers:")
+		fmt.Println("  - container1: Rotating")
+		fmt.Println("  - container2: Terminated")
+		fmt.Println("Messages: This is not kubectl, but cubectl!")
+	},
+}
+
+var describeCubesCmd = &cobra.Command{
+
+	Use:   "cubes",
+	Short: "Describe all cube",
+	Long:  `Show detailed information about all cubes (joke) in cubectl style.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		pods := []string{"cube-1", "cube-2", "cube-3"}
+		for _, pod := range pods {
+			fmt.Printf("Name: %s\n", pod)
+			fmt.Println("Namespace: default")
+			fmt.Println("Type: 3D Cube")
+			fmt.Println("Status: Rotating")
+			fmt.Println("Vertices: 8")
+			fmt.Println("Edges: 12")
+			fmt.Println("Faces: 6")
+			fmt.Println("Rotation: 0,0,0")
+			fmt.Println("Containers:")
+			fmt.Println("  - container1: Rotating")
+			fmt.Println("  - container2: Terminated")
+			fmt.Println("Messages: This is not kubectl, but cubectl!")
+			fmt.Println("")
+		}
 	},
 }
 
 func init() {
 	describeCmd.AddCommand(describeCubeCmd)
+	describeCmd.AddCommand(describeCubesCmd)
 }
